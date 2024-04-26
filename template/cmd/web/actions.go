23108ceaa595eac9d4a63f52751a247bc6099f35
Dmitry Petrov <dpetroff@gmail.com>
2024-04-26 02:18:16 +0200

    disable scaping, add actions

---
+++ template/cmd/web/actions.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+func setupActions(r *gin.RouterGroup, db boil.ContextExecutor) {
+}
+
+func reportError(c *gin.Context, s string) {
+	c.JSON(http.StatusBadRequest, gin.H{
+		"explanation": s,
+	})
+}
+
+func reportSuccess(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{})
+}
